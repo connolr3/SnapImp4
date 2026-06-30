3c4cddabb177da5b0fddaec3a7d618aec2423f31
Rosie Connolly <connolr3@tcd.ie>
2026-06-30 18:41:01 +0100

    UPDATED for next 10 participants

--- R STUDIO ANALYSIS/WILCOX.R
+++ R STUDIO ANALYSIS/WILCOX.R
@@ -1,6 +1,6 @@
 #install.packages("readxl")   # run once
 library(readxl)
-data <- read_excel("C:/Users/CONNOLR3/DOWNLOADS/test.xlsx")
+data <- read_excel("C:/Users/rosie/downloads/test.xlsx")
 library(tidyverse)
 library(ggpubr)
 library(tidyr)
@@ -10,11 +10,16 @@ library(rstatix)
 
 
 
-data$p_reg <data$`PSIAL REGULAR`
-data$p_snap <data$`PSIAL SNAP`
+data$p_reg <-data$`PSIAL REGULAR`
+data$p_snap <-data$`PSIAL SNAP`
 
 wilcox.test(data$adj_snap_av, data$adj_reg_av, paired=TRUE) 
 #p=0.2
+
+#0.008149
+
+
+#there is a significant difference 
 
 
 wilcox.test(
@@ -25,30 +30,32 @@ wilcox.test(
   exact = FALSE
 )
 #not significant
+#SIGNIFICANT
 
 
 
-
-
+data<-data[1:20,0:36]
 
 #Paired Samples T-TEST TO check for different in participants' preferred IPD and average IPD in baseline
-data$IPDDIFFERENCE<-data$`IPD Preference...19`-data$falseipd
+data$`false ipd`<-as.numeric(data$`false ipd`)
+data$`IPD Preference`<-as.numeric(data$`IPD Preference`)
+data$IPDDIFFERENCE<-data$`IPD Preference`-data$`false ipd`
 shapiro.test(data$IPDDIFFERENCE)
-t.test(data$`IPD Preference...19`, data$falseipd, paired = TRUE, alternative = "two.sided")
+t.test(data$`IPD Preference`, data$'false ipd', paired = TRUE, alternative = "two.sided")
+#SIGNIFICANT
 
-
-t.test(data$`IPD Preference...19`, data$falseipd,
+t.test(data$`IPD Preference`, data$'false ipd',
        paired = TRUE,
        alternative = "less")
 
 
-data$IPDDIFFERENCE<-data$`IPD Preference...19`-data$falseipd
-shapiro.test(data$IPDDIFFERENCE)
+data$IPDDIFFERENCE<-data$`IPD Preference`-data$'false ipd'
+shapiro.test(data$IPDDIFFERENCE)#NORMAL
 t.test(data$`IPD Preference...19`, data$falseipd, paired = TRUE, alternative = "two.sided")
 
 
-t.test(data$falseipd,data$`IPD Preference...19`,
+t.test(data$'false ipd',data$`IPD Preference`,
        paired = TRUE,
        alternative = "less")
-
+#true mean difference is less than 0
 
--- R STUDIO ANALYSIS/analysis.R
+++ R STUDIO ANALYSIS/analysis.R
@@ -1,6 +1,6 @@
 #install.packages("readxl")   # run once
 library(readxl)
-data <- read_excel("C:/Users/CONNOLR3/DOWNLOADS/test.xlsx")
+data <- read_excel("C:/Users/rosie/DOWNLOADS/test.xlsx")
 library(tidyverse)
 library(ggpubr)
 library(tidyr)
@@ -14,7 +14,7 @@ library(rstatix)
 
 #res.aov <- anova_test(data = data, dv = score, wid = id, within = time)
 #get_anova_table(res.aov)
-
+data<-data[1:20,0:32]
 data$p_reg <-data$`PSIAL REGULAR`
 data$p_snap <-data$`PSIAL SNAP`
 
@@ -23,8 +23,8 @@ long <- data %>%
   gather(key = "id", value = "score", , t2, t3) %>%
   convert_as_factor(id, time)
 
-
-shapiro.test(data$`PSIAL SNAP`)#normal
+#A p-value above 0.05 means the data are CONSISTENT with normal
+shapiro.test(data$`PSIAL SNAP`)#normal  (not significant, cannot reject normality
 
 shapiro.test(data$`PSIAL REGULAR`)#normal
 
@@ -33,14 +33,11 @@ diff <- data$`PSIAL SNAP` - data$`PSIAL REGULAR`
 shapiro.test(diff)#normal
 
 shapiro.test(data$adj_snap...21)
-diff <- data$`PSIAL SNAP` - data$`PSIAL REGULAR`
-shapiro.test(diff)
-
 
 
 
 data_long <- data %>%
-  rename(first_block = `first block`) %>%
+  rename(first_block = `first block...8`) %>%
   pivot_longer(
     cols = c(`PSIAL SNAP`, `PSIAL REGULAR`),
     names_to = "condition",
@@ -61,7 +58,7 @@ data_long %>%
   group_by(ID) %>%
   identify_outliers(PSIAL)
 
-
+#no outliers
 res.aov <- anova_test(data = data_long, dv = PSIAL, wid = ID, within = condition,between='first_block')
            
                       
@@ -83,8 +80,9 @@ anovamodel <- aov(
 
 
 ## T TEST
-
-t.test(data$falseipd, data$`IPD Preference...19`, paired = TRUE)
+data$`false ipd`<-as.numeric(data$`false ipd`)
+data$`IPD Preference`<-as.numeric(data$`IPD Preference`)
+t.test(data$`false ipd`, data$`IPD Preference`, paired = TRUE)#SIGNIFICANT
 
 
 
