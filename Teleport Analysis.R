737baabd8a3847498bbc1eb3fa7747b623bb7923
Rosie Connolly <60625969+connolr3@users.noreply.github.com>
2026-07-27 16:49:08 +0100

    new snap data path efficiency

--- Teleport Analysis.R
+++ Teleport Analysis.R
@@ -6,8 +6,15 @@ library(dplyr)
 library(rstatix)
 library(psych)
 library(nortest)
-setwd('C:/Users/rosie/OneDrive/Documents/GitHub/SnapImp4')
+library(ggplot2)
+
+
+
+#===================READ IN DATA====================
+setwd('C:/Users/connolr3/Documents/GitHub/SnapImp4')
 regData<-read.csv('regularteleportdata.csv')
+snapData<-read.csv('snapteleportdata.csv')
+
 
 
 
@@ -25,9 +32,9 @@ for (id in uniqueID) {
   for (teleport in uniqueTeleport) {
     print(teleport)
     print(id)
-    print(mag)
+    #print(mag)
     mag<-sum(regData[which(regData$Participant.ID == id & regData$Teleport == teleport ), 'distance.from.previous.with.begin'])
-  
+    
     magnitudes[[length(magnitudes)+1]] <- mag
   }
 }
@@ -49,20 +56,91 @@ teleport<-data.frame(temp,temp2,unlist(magnitudes),IPD)
 names(teleport)<-c('id','teleport','pathmag','final')
 
 
-teleport$teleportExcess<-teleport$final-teleport$pathmag
+teleport$teleportExcess<-teleport$pathmag-teleport$final
 plot(teleport$teleportExcess)
+plot(snapData$Excess)
+
+
+
+
+#write.csv(teleport,"RegularteleportExcess.csv", row.names = FALSE)
+
+
+
+
+
+test <- wilcox.test(snapData$TeleportExcess ~ teleport$teleportExcess)
+
+
+hist(snapData$Excess)
+shapiro.test(snapData$Excess)#fails test for normality
+shapiro.test(teleport$teleportExcess)#fails test for normality
+
+
+
+
+combinedTeleportData <- data.frame(
+  Time = c(rep("regular", 60), rep("snap", 60)),
+  Grade = c(teleport$teleportExcess, snapData$Excess)
+)
+combinedTeleportData
+
+
+
+combinedTeleportData$Time <- factor(combinedTeleportData$Time,
+                                    levels = c("regular", "snap")
+)
+
+ggplot(combinedTeleportData) +
+  aes(x = Time, y = Grade) +
+  geom_boxplot(fill = "#0c4c8a") +
+  theme_minimal()
+
+
+mean(snapData$Excess)
+mean(teleport$teleportExcess)
+
+
+
+
+
+# Testing our hypothesis
+library(MASS)
+wilcox.test(teleport$teleportExcess, snapData$Excess, paired=TRUE) 
+
+boxplot(teleport$teleportExcess, snapData$Excess)
 
 
 
 
 
 
+wilcox.test(teleport$teleportExcess, snapData$Excess, mu=0,alt="two.sided",conf.int=T, conf.level=0.99,paired=TRUE) 
 
 
 
 
+regmeans<-teleport%>%
+  group_by(id)%>%
+  summarize(mean = mean(teleportExcess))
+snapmeans<-snapData%>%
+  group_by(id)%>%
+  summarize(mean = mean(Excess))
+
+
+wilcox.test(regmeans$mean, snapmeans$mean, mu=0,alt="two.sided",conf.int=T, conf.level=0.99,paired=TRUE) 
 
 
 
+teleport$pathEfficiency<-teleport$final/teleport$pathmag
+
+snapData$Path.Efficiency<-as.numeric(snapData$Path.Efficiency)
+wilcox.test(teleport$pathEfficiency, as.numeric(snapData$Path.Efficiency), mu=0,alt="two.sided",conf.int=T, conf.level=0.99,paired=TRUE) 
+
+boxplot(teleport$pathEfficiency, snapData$Path.Efficiency)
+
+
+mean(teleport$pathEfficiency)
+mean(snapData$Path.Efficiency)
 
 
